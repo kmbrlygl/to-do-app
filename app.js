7af442906b3c7ee8d1d5a569b1c987d9917e7249
Kimberly Losenara <klosenara@gmail.com>
2018-10-19 20:32:12 -0700

    Add event handler

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
+function onReady() {
+    const addToDoForm = document.getElementById('addToDoForm');
+    const newToDoText = document.getElementById('newToDoText');
+    const toDoList = document.getElementById('toDoList');
+
+    addToDoForm.addEventListener('submit', () => {
+        event.preventDefault();
+
+        // get the text
+        let title = newToDoText.value;
+
+    });
+}
+
 window.onload = function() {
-    onreadystatechange();
+    onReady();
 };
 
