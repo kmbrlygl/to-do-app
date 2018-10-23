8317ee039be170f7da3a264a7a3b998a40dc471d
Kimberly Losenara <klosenara@gmail.com>
2018-10-23 15:11:02 -0700

    Got renderTheUI funct to work

--- app.js
+++ app.js
@@ -9,19 +9,16 @@ function onReady() {
             complete: false
         });
         newToDoText.value = '';
-    }
-    
-    addToDoForm.addEventListener('submit', event => {
-        event.preventDefault();
-        createNewToDo();
-    });
 
-    renderTheUI() {
+        renderTheUI();
+    }
+
+    function renderTheUI() {
         const toDoList = document.getElementById('toDoList');
 
         toDoList.textContent = '';
 
-        toDos.forEach(function(toDo)) {
+        toDos.forEach(function(toDo) {
             const newLi = document.createElement('li');
             const checkbox = document.createElement('input');
             checkbox.type = "checkbox";
@@ -32,6 +29,13 @@ function onReady() {
             newLi.appendChild(checkbox);
         });
     }
+    
+    addToDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createNewToDo();
+    });
+
+    renderTheUI();
 }
 
 window.onload = function() {
