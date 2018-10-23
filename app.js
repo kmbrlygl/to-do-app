12eb598313bbfe1304e3bf63f7948d95601fb88c
Kimberly Losenara <klosenara@gmail.com>
2018-10-23 14:34:21 -0700

    Set up initial sate, create to do functions

--- app.js
+++ app.js
@@ -1,36 +1,22 @@
 function onReady() {
+    const toDos = [];
     const addToDoForm = document.getElementById('addToDoForm');
-    const newToDoText = document.getElementById('newToDoText');
-    const toDoList = document.getElementById('toDoList');
 
-    addToDoForm.addEventListener('submit', () => {
-        event.preventDefault();
-
-        // get the text
-        let title = newToDoText.value;
-
-        // create a new li
-        let newLi = document.createElement('li');
-
-        // create a new input
-        let checkbox = document.createElement('input');
-
-        // set the input's type to checkbox 
-        checkbox.type = "checkbox";
-
-        // set the title 
-        newLi.textContent = title;
-
-        // attach the checkbox to the li
-        newLi.appendChild(checkbox);
-
-        // attach the li to the ul
-        toDoList.appendChild(newLi);
-
-        // empty the input
+    function createNewToDo() {
+        const newToDoText = document.getElementById('newToDoText');
+        toDos.push({
+            title: newToDoText.value,
+            complete: false
+        });
         newToDoText.value = '';
-
+    }
+    
+    addToDoForm.addEventListener('submit', event => {
+        event.preventDefault();
+        createNewToDo();
     });
+
+    renderTheUI();
 }
 
 window.onload = function() {
